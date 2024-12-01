2ea06367c9f7628b47c35fa33feca7a28b8025d4
vgrewal123 <varinder.singh.grewal@ibm.com>
2024-12-01 05:02:04 -0500

    deleted file

--- emotion_detection.py
+++
@@ -1,9 +0,0 @@
-import requests, json  # Import the requests library to handle HTTP requests
-
-def emotion_detector(text_to_analyse):  # Define a function named sentiment_analyzer that takes a string input (text_to_analyse)
-    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'  # URL of the sentiment analysis service
-    myobj = { "raw_document": { "text": text_to_analyse } }  # Create a dictionary with the text to be analyzed
-    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}  # Set the headers required for the API request
-    response = requests.post(url, json = myobj, headers=header)  # Send a POST request to the API with the text and headers
-
-    return response.text
